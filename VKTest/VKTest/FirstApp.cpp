4e6b67f62eb9364f2902139509b073191373fbdb
Chip Gallagher <cg699744@sju.edu>
2022-05-02 12:31:28 -0400

    TRIANGLE

--- VKTest/VKTest/FirstApp.cpp
+++ VKTest/VKTest/FirstApp.cpp
@@ -6,6 +6,7 @@ namespace vulkan {
 
 	FirstApp::FirstApp() {
 		createPipelineLayout();
+		createPipeline();
 		createCommandBuffers();
 	}
 
--- VKTest/VKTest/pipeline.cpp
+++ VKTest/VKTest/pipeline.cpp
@@ -65,7 +65,7 @@ namespace vulkan {
 		shaderStages[0].pNext = nullptr;
 		shaderStages[0].pSpecializationInfo = nullptr;
 		shaderStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-		shaderStages[1].stage = VK_SHADER_STAGE_VERTEX_BIT;
+		shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
 		shaderStages[1].module = fragShader;
 		shaderStages[1].pName = "main";
 		shaderStages[1].flags = 0;
@@ -96,6 +96,7 @@ namespace vulkan {
 		pipelineInfo.pInputAssemblyState = &configInfo.inputAssemblyInfo;
 		pipelineInfo.pViewportState = &configInfo.viewportInfo;
 		pipelineInfo.pRasterizationState = &configInfo.rasterizationInfo;
+		pipelineInfo.pMultisampleState = &configInfo.multisampleInfo;
 		pipelineInfo.pColorBlendState = &configInfo.colorBlendInfo;
 		pipelineInfo.pDepthStencilState = &configInfo.depthStencilInfo;
 		pipelineInfo.pDynamicState = nullptr;
